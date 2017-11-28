7636e00381b5b808288c90cdbb40dc1888636f4f
Eric <eric.fumarola@fravega.com.ar>
2017-11-28 13:30:55 -0300

    281117s1

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,11 +1,34 @@
 import React, {Component} from 'react';
+import ListContacts from './ListContacts'
+
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+]
 
 class App extends Component {
 
   render() {
-    return (<div className="App">
-      <div>Hello world</div>
-    </div>);
+    return (
+      <div className="App">
+        <ListContacts contacts={contacts}/>
+      </div>);
   }
 }
 
